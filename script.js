bf73af7bfef71aa3846d3588bf5b5e3008a5d228
a.zagoveliev <63207977+mexell96@users.noreply.github.com>
2020-05-25 16:09:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,65 @@
 // #1
-const header = document.getElementById('header');
-header.addEventListener('click', function (ev) {
-   if (ev.target.nodeName === "LI") {
-   ev.target.classList.add('beader');
-   }
-});
+const numbers = [100, 101, 102, 103];
 
-const pop = document.getElementsByTagName('li');
-
-const veader = document.getElementById('veader');
-veader.addEventListener('click', function (header) {
-   if (header.target.nodeName !== "LI") {
-      for (let i = 0; i < pop.length; i++) {
-         pop[i].classList.remove('beader');
-      }
-   }
-});
+for (var i = 0; i < numbers.length; i++) {
+    let number = numbers[i];
+    let indexEl = i;
+  setTimeout(function() {
+    console.log('Index: ' + indexEl + ', element: ' + number);
+  }, (i + 1) * 1000);
+}
 
 
 // #2
-const lishki = document.getElementById('classs1');
-lishki.addEventListener('click', function (ev) {
-   if (ev.target.nodeName === 'LI') {
-   ev.target.classList.add('beader');
-   }
-});
+const numbers = [100, 101, 102, 103]
 
-const pop2 = document.getElementsByTagName('li');
+numbers.forEach(function(el, i) {
+  setTimeout(function() {
+    console.log('Index: ' + i + ', element: ' + el)
+  }, (i + 1) * 1000)
+})
 
-const veader2 = document.getElementById('veader');
-veader2.addEventListener('click', function (header) {
-   if (header.target.nodeName !== "LI") {
-      for (let i = 0; i < pop2.length; i++) {
-         pop2[i].classList.remove('beader');
-      }
-   }
-});
 
-lishki.addEventListener('click', function (ev) {
-   if (ev.target.nodeName === 'LI') {
-      let wer = ev.target; 
-      let fyt = wer.firstChild;
-      let rev = fyt.textContent;
-      alert(rev);
-   }
-});
+
+
+// #2
+const items = [
+    { 
+        value: 'child_1',
+        parents: [{
+            value: 'parent_1',
+            parents: [{
+                value: 'grandparent_1',
+                parents: [],
+            }],
+        },
+        {
+            value: 'parent_2',
+            parents: [{
+                value: 'grandparent_2',
+                parents: [{
+                    value: 'grandgrandparent_2',
+                    parents: null,
+                }],
+            }],
+        }],
+    },
+    { 
+        value: 'child_2',
+        parents: null,
+    }
+];
+
+function func(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (typeof arr [i] == 'object') {
+            console.log(arr[i].value);
+        } else {
+            arr [i] = func(arr[i]);
+        }
+    }
+    return arr;
+}
+
+func(items);
+// не понятно как получить значения из вложенных объектов
